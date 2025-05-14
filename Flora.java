6c9db3e66d06a65d0dcfb506ed1868a9deda36cb
faddwaaa <fadwapamulasih@gmail.com>
2025-05-14 11:56:54 +0700

    menambahkan 3 file java

---
+++ Flora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulasan;
+
+/**
+ *
+ * @author Fadwa Pamulasih
+ */
+public class Flora {
+//    property / variabel
+//    method
+//    modifier
+    String nama = "Kelengkeng", buah = "Rambutan", batang;
+    int usia;
+    
+    
+}
